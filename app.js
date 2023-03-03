a03b6e8b22bc59fada1cd031c255c222ebb8f389
Ken Graf <kengraf57@gmail.com>
2023-03-03 16:08:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ const quotes = [
   'You\'re capable of more than you can even dream.',
   'You deserve the best.',
   'Keep going, you\'re doing well.',
-  'Stay positive; stay hopeful'
+  'Stay positive; stay hopeful.',
+  'Open your heart and drink in this glorious day.',
+  'Am I good enough? Yes i am.',
+  'The perfect moment is this one.',
+  'Life is about to be incredible.',
+  'Nothing can dim the light that shines from within.',
+  'You must do things that you think you cannot do.',
+  'The secret of attraction is to love yourself.',
+  'Look on the bright side, it doesn’t hurt anyone.',
+  'Think big and your world will be big.',
+  'Embrace the glorious mess that you are.',
+  'Nothing is impossible the word itself say “I’m possible”.',
+  'The only courage you ever need is the courage to fulfill the dreams of your own life.',
+  'Write it on your heart that every day is the best day in the year.',
+  'Hold up your head! You were not made for failure, you were made for victory.',
+  'If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely.'
+
 ];
 
 async function displayQuote() {
